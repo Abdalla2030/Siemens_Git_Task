3bf1e62670d83add42a3ce5f79c2f456bffcbaca
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2023-03-11 00:12:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 
+int  getSum(vector <int> numbers){
+    int sum = 0 ;
+    for(int i = 0 ;i<numbers.size();i++){
+        sum += numbers[i];
+    }
+    return sum ;
+}
+
 double getAverage(vector <int> numbers){
     int sum = 0 ;
     double avg = 0 ;
@@ -17,6 +25,7 @@ double getAverage(vector <int> numbers){
 
 int main(){
     vector <int> numbers = {1,2,3,4,5,6,7,8,9,10};
+    cout<<getSum(numbers)<<endl;
     cout<<getAverage(numbers)<<endl;
 
     return 0;
