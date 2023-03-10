ad82269e768772a2e062e00ceb9f3e365eafa5b8
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2023-03-11 00:05:40 +0200

    getMin Function

--- main.cpp
+++ main.cpp
@@ -3,21 +3,18 @@
 using namespace std;
 
 
-double getAverage(vector <int> numbers){
-    int sum = 0 ;
-    double avg = 0 ;
+int getMin(vector <int> numbers){
+    int mn = INT_MAX ;
     for(int i = 0 ;i<numbers.size();i++){
-        sum += numbers[i];
+        mn = min(mn,numbers[i]);
     }
-    avg =(double) sum / numbers.size();
-    return avg ;
+    return mn ;
 }
-
 
 
 int main(){
     vector <int> numbers = {1,2,3,4,5,6,7,8,9,10};
-    cout<<getAverage(numbers)<<endl;
+    cout<<getMin(numbers)<<endl;
 
     return 0;
 }
