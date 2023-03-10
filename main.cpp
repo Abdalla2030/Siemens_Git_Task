16722776bc57dd7737a2beb6c78abb45aaeca6c7
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2023-03-11 00:04:21 +0200

    getSum Function

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int  getSum(vector <int> numbers){
+    int sum = 0 ;
+    for(int i = 0 ;i<numbers.size();i++){
+        sum += numbers[i];
+    }
+    return sum ;
+}
+
+
+
+int main(){
+    vector <int> numbers = {1,2,3,4,5,6,7,8,9,10};
+    cout<<getSum(numbers)<<endl;
+    return 0;
+}
