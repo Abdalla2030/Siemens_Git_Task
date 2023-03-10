2f8c6e929ab7436cb98ab1d9f4ab4078ab7f29a2
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2023-03-11 00:05:12 +0200

    getAverage Function

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 #include <vector>
 using namespace std;
 
-int  getSum(vector <int> numbers){
+
+double getAverage(vector <int> numbers){
     int sum = 0 ;
+    double avg = 0 ;
     for(int i = 0 ;i<numbers.size();i++){
         sum += numbers[i];
     }
-    return sum ;
+    avg =(double) sum / numbers.size();
+    return avg ;
 }
 
 
 
 int main(){
     vector <int> numbers = {1,2,3,4,5,6,7,8,9,10};
-    cout<<getSum(numbers)<<endl;
+    cout<<getAverage(numbers)<<endl;
+
     return 0;
 }
